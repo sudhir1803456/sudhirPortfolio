5552ed5342843894bf02f1f927471989b5b672e7
Sudhir Kumar <kumarsudhir804435@gmail.com>
2026-03-18 21:09:24 +0530

    initial portfolio

---
+++ script.js
@@ -0,0 +1,52 @@
+import './style.css';
+import emailjs from '@emailjs/browser';
+
+const btn = document.getElementById("revealBtn");
+const info = document.getElementById("info");
+
+let isOpen = false;
+// init EmailJS
+(function () {
+  emailjs.init("CZPz38rF2tJA1tfEz");
+})();
+btn.addEventListener("click", () => {
+  if (!isOpen) {
+    // SHOW
+    info.classList.remove("hidden");
+
+    setTimeout(() => {
+      info.classList.add("show");
+    }, 50);
+
+    btn.innerText = "Hide Details";
+    isOpen = true;
+
+  } else {
+    // HIDE
+    info.classList.remove("show");
+
+    setTimeout(() => {
+      info.classList.add("hidden");
+    }, 400);
+
+    btn.innerText = "Know More About Me";
+    isOpen = false;
+  }
+});
+const form = document.getElementById("contactForm");
+const msg = document.getElementById("successMsg");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  emailjs.sendForm("service_nlslxgj", "template_yiog9ne", form)
+    .then(() => {
+        alert("✅ Query sent successfully!");
+      form.reset();
+    })
+    .catch(() => {
+          alert("❌ Failed to send. Try again!");
+        form.reset();
+
+    });
+});
